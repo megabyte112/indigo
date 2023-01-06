2de98ec5f6d59d401b8e1cfb67aae6c3c2e84503
megabyte112 <>
2023-01-06 20:01:29 +0000

    fix lsb-release

--- airootfs/etc/profile.d/start.sh
+++ airootfs/etc/profile.d/start.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+# clone os-release
 ln -f /etc/os-release /usr/lib/os-release
+
+#overwrite arch's lsb-release
+echo "DISTRIB_ID='Indigo'
+DISTRIB_RELEASE='rolling'
+DISTRIB_DESCRIPTION='Indigo Linux'" >> /etc/lsb-release
+
+
 clear
+neofetch
 startx
