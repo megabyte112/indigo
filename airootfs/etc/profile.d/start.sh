78d1ab597a18f11ea8885445bb5cc5ea9303e5ea
megabyte112 <>
2023-01-08 21:09:39 +0000

    maybe hopefully fix install perms??

--- airootfs/etc/profile.d/start.sh
+++ airootfs/etc/profile.d/start.sh
@@ -3,9 +3,12 @@
 # clone os-release
 ln -f /etc/os-release /usr/lib/os-release
 
-#overwrite arch's lsb-release
+# overwrite arch's lsb-release
 rm -f /etc/lsb-release
 echo "DISTRIB_ID='Indigo'
 DISTRIB_RELEASE='rolling'
 DISTRIB_DESCRIPTION='Indigo Linux'" >> /etc/lsb-release
+
+# make install script executable
+chmod +x /root/install
 
