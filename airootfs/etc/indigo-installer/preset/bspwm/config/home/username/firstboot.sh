37af162e26fcd7fca514956954219abd9aaf1bb7
megabyte112 <>
2023-03-31 21:13:18 +0100

    fix more stuff

--- airootfs/etc/indigo-installer/preset/bspwm/config/home/username/firstboot.sh
+++ airootfs/etc/indigo-installer/preset/bspwm/config/home/username/firstboot.sh
@@ -4,7 +4,7 @@ betterlockscreen -u /usr/share/backgrounds/waves.png
 # set background
 feh --bg-scale /usr/share/backgrounds/waves.png
 
-# uncomend '$HOME/.fehbg &'' in bspwmrc
+# uncoment '$HOME/.fehbg &'' in bspwmrc
 sed -i "s/#$HOME\/.fehbg &/$HOME\/.fehbg &/" $HOME/.config/bspwm/bspwmrc
 
 # in bspwmrc, remove 'bash $HOME/firstboot.sh' and replace with 'betterlockscreen -l dim'
