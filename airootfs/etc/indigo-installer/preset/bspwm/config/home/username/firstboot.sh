df47995158551a4814f2749ac97dd88bafe68690
megabyte112 <>
2023-04-05 21:23:26 +0100

    add welcome screen with keybindings for bspwm

--- airootfs/etc/indigo-installer/preset/bspwm/config/home/username/firstboot.sh
+++ airootfs/etc/indigo-installer/preset/bspwm/config/home/username/firstboot.sh
@@ -4,11 +4,11 @@ betterlockscreen -u /usr/share/backgrounds/waves.png
 # set background
 feh --bg-scale /usr/share/backgrounds/waves.png
 
-# uncoment '$HOME/.fehbg &'' in bspwmrc
-sed -i "s/#\$HOME\/.fehbg &/\$HOME\/.fehbg &/" $HOME/.config/bspwm/bspwmrc
-
 # in bspwmrc, remove 'bash $HOME/firstboot.sh' and replace with 'betterlockscreen -l dim
 sed -i "s/bash \$HOME\/firstboot.sh/betterlockscreen -l dim/" $HOME/.config/bspwm/bspwmrc
+
+# open keybind file
+alacritty -e sh -c "cat $HOME/keybinds | less"
 
 # lock screen
 betterlockscreen -l dim
