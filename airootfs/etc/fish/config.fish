060dcf06f16fa17de0a5a4e06bb5793b20cae0fd
megabyte112 <>
2023-04-10 18:54:54 +0100

    added startup commands to fish config

--- airootfs/etc/fish/config.fish
+++ airootfs/etc/fish/config.fish
@@ -1,1 +1,17 @@
+# remove fish greeting
 set fish_greeting
+
+# clone os-release
+ln -sf /etc/os-release /usr/lib/os-release
+
+# overwrite arch's lsb-release
+rm -f /etc/lsb-release
+echo "DISTRIB_ID='Indigo'
+DISTRIB_RELEASE='rolling'
+DISTRIB_DESCRIPTION='Indigo Linux'" >> /etc/lsb-release
+
+# make install script executable
+chmod +x /etc/indigo-installer/indigoinstall
+
+# make symlink from installer to /root/install
+ln -sf /etc/indigo-installer/indigoinstall /root/install
