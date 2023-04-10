230cd802d26905029d455713921c63159531cce4
megabyte112 <>
2023-04-10 18:45:04 +0100

    added default fish config

--- airootfs/etc/profile.d/start.sh
+++ airootfs/etc/profile.d/start.sh
@@ -15,6 +15,3 @@ chmod +x /etc/indigo-installer/indigoinstall
 # make symlink from installer to /root/install
 ln -sf /etc/indigo-installer/indigoinstall /root/install
 
-# remove fish welcome message
-echo "set fish_greeting" >> /etc/fish/config.fish
-
